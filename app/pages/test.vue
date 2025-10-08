f7eb60c4acfe3b48c52ffa8ac769f67deb755791
Jeremy <29926331+j3rr7@users.noreply.github.com>
2025-10-08 16:49:23 +0700

    .

--- app/pages/test.vue
+++ app/pages/test.vue
@@ -2,7 +2,9 @@
 import TableCompany from "~/components/table/TableCompany.vue";
 import useCompanies from "~/composable/useCompanies";
 
-const { companies, fetchCompanies } = useCompanies();
+const { companies, searchQuery, fetchCompanies } = useCompanies();
+
+const isCreateDialogOpen = ref(false);
 
 onMounted(() => {
   fetchCompanies();
@@ -11,23 +13,71 @@ onMounted(() => {
 
 <template>
   <div class="max-w-7xl mx-auto">
-    <DialogCompanyCreate 
-      @created="() => {console.log('created')}"
-      @cancel="() => {console.log('cancel')}"
-    />
+    <div class="flex justify-between items-center mb-6">
+      <UInput
+        v-model="searchQuery"
+        icon="i-lucide-search"
+        variant="outline"
+        type="text"
+        placeholder="Search by company name or code..."
+        :ui="{
+          base: 'min-w-[200px] sm:min-w-[300px] md:min-w-[400px] lg:min-w-[500px] w-full',
+        }"
+      />
+      <UButton icon="i-heroicons-plus-circle" @click="isCreateDialogOpen = true"
+        >Create Company</UButton
+      >
+    </div>
 
-    <DialogCompanyExtend 
+    <UModal v-model:open="isCreateDialogOpen" :ui="{ wrapper: 'sm:max-w-xl' }">
+      <template #content>
+        <DialogCompanyCreate
+          @created="
+            () => {
+              console.log('created');
+            }
+          "
+          @cancel="isCreateDialogOpen = false"
+        />
+      </template>
+    </UModal>
+
+    <DialogCompanyExtend
       :company="companies[0]!"
-      @extended="() => {console.log('extended')}"
-      @cancel="() => {console.log('cancel')}"
+      @extended="
+        () => {
+          console.log('extended');
+        }
+      "
+      @cancel="
+        () => {
+          console.log('cancel');
+        }
+      "
     />
 
-    <TableCompany 
-      :companies="companies" 
-      @extend="() => {console.log('extend')}" 
-      @disable="() => {console.log('disable')}"
-      @enable="() => {console.log('enable')}"
-      @delete="() => {console.log('delete')}"
+    <TableCompany
+      :companies="companies"
+      @extend="
+        () => {
+          console.log('extend');
+        }
+      "
+      @disable="
+        () => {
+          console.log('disable');
+        }
+      "
+      @enable="
+        () => {
+          console.log('enable');
+        }
+      "
+      @delete="
+        () => {
+          console.log('delete');
+        }
+      "
     />
   </div>
 </template>
